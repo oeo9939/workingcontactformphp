4d649eabc900018bff4ce670961fca0aa35b4314
Teo <mgttmich@gmail.com>
2022-12-21 20:28:55 +0200

    Add form.onsubmit

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault(); // preventing form submitting
+    statusTxt.style.display = "block";
+
+    let xhr = new XMLHttpRequest(); // creating new xml object
+    xhr.open("POST", "message.php", true); // sending post request to message.php file
+    xhr.onload = () => { // once ajax loaded
+        if (xhr.readyState == 4 && xhr.status == 200) { // if ajax response status is 200 & read status is 4 it means there is no error
+            let response = xhr.response; // storing ajax response in a response variable
+            console.log(response);
+        }
+    }
+    xhr.send();
 }
+
